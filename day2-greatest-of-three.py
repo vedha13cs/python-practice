ab8345787d526a4865ffa18488f899732f343443
Vedhavathi Y A <vedha9782@gmail.com>
2026-02-11 23:25:42 +0530

    Refactor greatest of three numbers logic
    Updated the logic to find the greatest of three numbers with new variable names and input prompts.

--- day2-greatest-of-three.py
+++ day2-greatest-of-three.py
@@ -1,10 +1,11 @@
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-c = int(input("Enter third number: "))
-
-if a >= b and a >= c:
-    print("Greatest number is:", a)
-elif b >= a and b >= c:
-    print("Greatest number is:", b)
+num6=int(input(" Enter the first number:"))
+num7=int(input(" Enter the secnd number "))
+num8=int(input("Enter the third number : "))
+if( num6>num7 and num6>num8):
+    print(num6,"is greater")
+elif( num7>num8 and num7>num6):
+    print(num7,"is greater")
+elif(num8>num6 and num6>num8):
+    print( num8,"is greater")
 else:
-    print("Greatest number is:", c)
+    print("Invalid option")
