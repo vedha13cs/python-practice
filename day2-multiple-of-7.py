f7f2784845fa17fd872a463d82f8f9b09ad8977b
Vedhavathi Y A <vedha9782@gmail.com>
2026-02-11 23:40:22 +0530

    Fix print statement formatting for non-multiple of 7

--- day2-multiple-of-7.py
+++ day2-multiple-of-7.py
@@ -2,4 +2,4 @@ com=int(input("Enter the Number:"))
 if(com %7 ==0 ):
     print("Given number",com,"is multiple of 7")
 else:
-    print("The given number",com,"is not a multiple of 7")
+    print("Given number",com,"is not a multiple of 7")
