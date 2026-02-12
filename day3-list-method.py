bbe21ebbc366dd1f2823ad231cb4dc0dd0400032
Vedhavathi Y A <vedha9782@gmail.com>
2026-02-12 23:17:29 +0530

    Implement list methods for number manipulation

---
+++ day3-list-method.py
@@ -0,0 +1,9 @@
+number=[1,3,5,7,9,0,]
+number.append(4)
+print("after append :",number)
+number.insert(1,5)
+print("After insert :",number)
+number.sort()
+print("Sorted list :",number)
+number.reverse()
+print("Reversed list :",number )
