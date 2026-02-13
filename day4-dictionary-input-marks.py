431b635bff2d3749babd3b8a7b242fea278caea3
Vedhavathi Y A <vedha9782@gmail.com>
2026-02-13 20:37:21 +0530

    Add input for subject marks and store in dictionary

---
+++ day4-dictionary-input-marks.py
@@ -0,0 +1,8 @@
+marks={}
+x=int(input(" Enter the marks of subject one :"))
+marks.update({ "phy": x})
+y=int(input(" Enter the marks of subject two :"))
+marks.update({ "chem": y})
+z=int(input(" Enter the marks of subject three :"))
+marks.update({ "maths": z})
+print(marks)
